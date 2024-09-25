ae67d3e2470ac5b325fc0586f108f7fd26d77c55
arnold rodriguez <arnolddavidrd@gmail.com>
2024-09-24 19:40:14 -0500

    comentarios

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,14 +4,18 @@ import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 
 export default function Layout() {
+  /* obtener el estado de la navegación */
   const navigation = useNavigation();
+
   return (
     <div>
+      {/* mostrar un loading mientras carga la navegación */}
       {navigation.state === "loading" && (
         <div className="fixed top-0 left-0 w-full h-full bg-white bg-opacity-50 z-50 flex justify-center items-center">
           <p className="text-center text-4xl font-bold">Cargando...</p>
         </div>
       )}
+
       <Header />
       <Nav />
       <Outlet />
