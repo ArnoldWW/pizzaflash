bfe57bcf5b952a35ef7a5aaf9e8b876dc2cad869
arnold rodriguez <arnolddavidrd@gmail.com>
2024-10-02 18:40:52 -0500

    navegacion responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 import { logOut } from "../firebase/auth";
@@ -21,6 +21,9 @@ const LINKS = [
 ];
 
 export default function Nav() {
+  /* estado para mostrar o no el menú en movil */
+  const [showMenu, setShowMenu] = useState(false);
+
   /* obtener el usuario logueado */
   const { user, setUser } = useContext(AuthContext);
   /* obtener el estado del carrito */
@@ -33,43 +36,23 @@ export default function Nav() {
   };
 
   return (
-    <nav className="max-w-[1000px] mx-auto w-[90%] py-5 flex md:flex-row flex-col justify-between items-center font-bold uppercase gap-5">
-      <div className="flex md:flex-row flex-col justify-start gap-5">
-        {LINKS.map((link) => (
-          <NavLink
-            key={link.href}
-            to={link.href}
-            className={({ isActive, isPending }) =>
-              isPending
-                ? "opacity-50"
-                : isActive
-                ? "text-orange-600"
-                : "hover:underline"
-            }
-          >
-            {link.text}
-            {link.href === "/cart" && (
-              <span className="ml-1">({cart.length})</span>
-            )}
-          </NavLink>
-        ))}
-      </div>
-      <div className="flex md:flex-row flex-col justify-end gap-5">
-        {user ? (
-          <div className="flex items-center gap-5">
-            <p>Hola, {user?.displayName}</p>
-            <button
-              type=" button"
-              className="btn-orange"
-              onClick={handleClickLogout}
-            >
-              Cerrar sesión
-            </button>
-          </div>
-        ) : (
-          <>
+    <>
+      <button
+        className="block md:hidden btn-black my-5 mx-auto"
+        onClick={() => setShowMenu(!showMenu)}
+      >
+        Mostrar menú
+      </button>
+      <nav
+        className={`${
+          showMenu ? "flex" : "hidden"
+        } max-w-[1000px] mx-auto w-[90%] md:py-5 pb-5 md:flex md:flex-row flex-col justify-between items-center font-bold uppercase gap-5`}
+      >
+        <div className="flex md:flex-row flex-col justify-center items-center gap-5">
+          {LINKS.map((link) => (
             <NavLink
-              to="/login"
+              key={link.href}
+              to={link.href}
               className={({ isActive, isPending }) =>
                 isPending
                   ? "opacity-50"
@@ -78,23 +61,55 @@ export default function Nav() {
                   : "hover:underline"
               }
             >
-              Iniciar Sesión
+              {link.text}
+              {link.href === "/cart" && (
+                <span className="ml-1">({cart.length})</span>
+              )}
             </NavLink>
-            <NavLink
-              to="/signup"
-              className={({ isActive, isPending }) =>
-                isPending
-                  ? "opacity-50"
-                  : isActive
-                  ? "text-orange-600"
-                  : "hover:underline"
-              }
-            >
-              Registrarse
-            </NavLink>
-          </>
-        )}
-      </div>
-    </nav>
+          ))}
+        </div>
+        <div className="flex md:flex-row items-center justify-center flex-col gap-5">
+          {user ? (
+            <div className="flex items-center gap-5">
+              <p>Hola, {user?.displayName}</p>
+              <button
+                type=" button"
+                className="btn-orange"
+                onClick={handleClickLogout}
+              >
+                Cerrar sesión
+              </button>
+            </div>
+          ) : (
+            <>
+              <NavLink
+                to="/login"
+                className={({ isActive, isPending }) =>
+                  isPending
+                    ? "opacity-50"
+                    : isActive
+                    ? "text-orange-600"
+                    : "hover:underline"
+                }
+              >
+                Iniciar Sesión
+              </NavLink>
+              <NavLink
+                to="/signup"
+                className={({ isActive, isPending }) =>
+                  isPending
+                    ? "opacity-50"
+                    : isActive
+                    ? "text-orange-600"
+                    : "hover:underline"
+                }
+              >
+                Registrarse
+              </NavLink>
+            </>
+          )}
+        </div>
+      </nav>
+    </>
   );
 }
